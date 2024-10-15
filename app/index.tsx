6d480e864cb19891b72f087cbab2f712cc064377
Claudio <claudiogsssantos@gmail.com>
2024-10-15 16:27:53 -0300

    ajustes

--- app/index.tsx
+++ app/index.tsx
@@ -1,13 +1,11 @@
 import { Text, View, StyleSheet, Image, Pressable } from "react-native";
-import { colors } from "../constants/colors"
-import { Link } from 'expo-router'
+import { colors } from "../constants/colors";
+import { Link } from "expo-router";
 
 export default function Index() {
   return (
     <View style={styles.screen}>
-      <Image
-        source={require('../assets/images/nialogo.png')}
-      />
+      <Image source={require("../assets/images/nialogo.png")} />
       <Text style={styles.title}>NIA Diet</Text>
       <Text style={styles.subtitle}>Sua dieta personalizada</Text>
       <Link href="/stage" asChild>
@@ -20,35 +18,35 @@ export default function Index() {
 }
 
 const styles = StyleSheet.create({
-  screen:{
+  screen: {
     backgroundColor: colors.green,
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    paddingHorizontal: 16
+    alignItems: "center",
+    justifyContent: "center",
+    paddingHorizontal: 16,
   },
 
   button: {
     borderRadius: 10,
     padding: 12,
     backgroundColor: colors.orange,
-    width: '100%',
+    width: "100%",
   },
   btnText: {
     color: colors.white,
-    justifyContent: 'center',
-    alignItems: 'center',
-    textAlign: 'center'
+    justifyContent: "center",
+    alignItems: "center",
+    textAlign: "center",
   },
   title: {
     fontSize: 64,
     color: colors.white,
     fontStyle: "italic",
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
   subtitle: {
     fontSize: 24,
     color: colors.white,
-    paddingVertical: 24,
-  }
-})
+    paddingVertical: 12,
+  },
+});
