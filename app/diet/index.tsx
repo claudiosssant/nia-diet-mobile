f3729c68542eec0873e8db6d0a3767f4e3ad9f42
Claudio <claudiogsssantos@gmail.com>
2024-11-03 14:51:41 -0300

    feat: creating service

--- app/diet/index.tsx
+++ app/diet/index.tsx
@@ -1,6 +1,11 @@
 import { View, Text, StyleSheet } from "react-native";
+import { useDataStore } from "../../store/data";
+
 
 export default function DietResult() {
+  const user = useDataStore(state => state.user)
+
+  console.log(user)
   return(
     <View style={styles.main}>
       <Text> TEstando </Text>
